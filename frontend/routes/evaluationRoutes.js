b2bc825542ce683df799479c8b799558ff296729
José Daniel <91283818+daniellsilva3@users.noreply.github.com>
2024-12-06 01:49:01 -0300

    Delete frontend directory

--- frontend/routes/evaluationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getEvolutions, createEvolution } = require('../controllers/evaluationController');
-
-// Rotas para evoluções
-router.get('/', getEvolutions); // Lista evoluções
-router.post('/', createEvolution); // Cria uma nova evolução
-
-module.exports = router;
